66d8ee6c400ee23e1c5a2e2c8db62f7ee3d63b03
Azeez O. Odu <az6bcn@gmail.com>
2019-01-15 21:16:54 +0000

    NavBar Component Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
