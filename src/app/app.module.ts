bdf0826c37be1b5af5af8caf22a98b54c87a4f42
Azeez O. Odu <az6bcn@gmail.com>
2019-01-15 21:54:28 +0000

    NavBar with items,routing, Ngx-Bootstrap configured, its ddp used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { Router, RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,11 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot([
+      {path: 'shopping-cart', component: ShoppingCartComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
